f7a78a376558cb3fc158af2c7f293b885e2ab314
RosaryAbilash <75167373+RosaryAbilash@users.noreply.github.com>
2022-07-18 15:32:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,17 +14,17 @@
                     <button type="button" class="toggle-btn" onclick="signup()">Signup</button>
                 </div>
                 <form id="login" class="input" action="dashboard.php" method="post">
-                    <input type="text" class="input-field" placeholder="Email Id" required>
-                    <input type="text" class="input-field" placeholder="Password" required>
+                    <input type="text" name="email" class="input-field" placeholder="Email Id" required>
+                    <input type="text" name="passwd" class="input-field" placeholder="Password" required>
                     <input type="checkbox" class="checkbox"><span>Remember Password</span>
                     <button type="submit" class="submit-btn">Login</button>
                 </form>
                 <form id="signup" class="input"  action="connect.php" method="post">
-                    <input type="text" class="input-field" placeholder="Name" required>
-                    <input type="number" class="input-field" placeholder="Age" required>
-                    <input type="date" class="input-field" placeholder="DOB" required>
-                    <input type="email" class="input-field" placeholder="Email Id" required>
-                    <input type="password" class="input-field" placeholder="Password" required>
+                    <input type="text" name="uname"class="input-field" placeholder="Name" required>
+                    <input type="number" name="age" class="input-field" placeholder="Age" required>
+                    <input type="date" name="dob" class="input-field" placeholder="DOB" required>
+                    <input type="email" name="email" class="input-field" placeholder="Email Id" required>
+                    <input type="password" name="passwd" class="input-field" placeholder="Password" required>
                     <button type="submit" class="submit-btn">Signup</button>
                 </form>
             </div>
